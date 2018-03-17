3916f91e7748bf6807f8321c5247bb0acfe5d576
gyy150 <gyy150@163.com>
2018-03-17 17:41:02 +0800

    First commit, added CSV reader, splitting to test and training set

--- Test.py
+++ Test.py
@@ -6,7 +6,8 @@ with open("../Data_Set/Training.txt", "w") as training_text_file:
         with open('../Data_Set/loader_work_orders_sanitised.csv', newline='') as csvfile:
                 data_reader = csv.reader(csvfile)
                 for row in data_reader:
-                    print(row[4])
+                    short_text = row[4]
+                    print(short_text.split())
                     dice = randrange(4)
                     if dice > 3:
                         print(row[4], file=testing_text_file)
